f9851490c7481e6c686bdbea5e36bbe85994dde7
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-14 11:42:39 -0300

    feat: Add VTextField component

---
+++ src/shared/components/Forms/VTextField/index.tsx
@@ -0,0 +1,32 @@
+import { TextField, TextFieldProps } from '@mui/material'
+import { Controller } from 'react-hook-form'
+
+type TVTextFieldProps = TextFieldProps & {
+  name: string
+  label: string
+  control?: any
+}
+
+export const VTextField: React.FC<TVTextFieldProps> = ({
+  name,
+  label,
+  control,
+  ...rest
+}) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { value, onChange }, fieldState: { error } }) => (
+        <TextField
+          {...rest}
+          label={label}
+          value={value}
+          onChange={onChange}
+          helperText={error ? error.message : null}
+          error={!!error}
+        />
+      )}
+    />
+  )
+}
