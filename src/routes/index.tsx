1f19579e2df6eb05fc154188f1d556278596df06
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-15 16:25:47 -0300

    feat: Redirect Authenticated Users

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { useAppSelector } from '../shared/hooks'
 import { Home, Chat } from '../pages'
 
 export const AppRoutes = () => {
-  const { auth } = useAppSelector(state => state.user)
+  const auth = useAppSelector(state => state.user.auth)
 
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/chat" element={auth ? <Chat /> : <Home />} />
+      <Route path="/home" element={auth ? <Navigate to="/chat" /> : <Home />} />
+      <Route path="/chat" element={auth ? <Chat /> : <Navigate to="/home" />} />
 
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={!auth ? <Home /> : <Navigate to="/chat" />} />
     </Routes>
   )
 }
