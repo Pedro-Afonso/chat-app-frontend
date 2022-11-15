d067c82c503104955e5dccee81d52b3f2bd764c1
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-15 00:13:15 -0300

    feat: Implements registration functionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Box } from '@mui/material'
 import { LoginForm } from '../../shared/components/LoginForm'
 import { RegisterForm } from '../../shared/components/RegisterForm'
 import { FullWidthTabs } from '../../shared/components'
+import { useAppDispatch } from '../../shared/hooks'
+import { register } from '../../shared/slices/userSlice'
 
 interface IFormProps {
   name: string
@@ -11,12 +13,17 @@ interface IFormProps {
 }
 
 export const Home = () => {
+  const dispatch = useAppDispatch()
+
   const handleLogin = (data: Omit<IFormProps, 'name' | 'confirmPassword'>) => {
     console.log(data)
   }
 
   const handleRegister = (data: IFormProps) => {
+    const { name, email, password, confirmPassword } = data
     console.log(data)
+
+    dispatch(register(data))
   }
 
   return (
