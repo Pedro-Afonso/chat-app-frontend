1a2241ed8b33a834ccfaecf7756ee7200a72df68
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-15 16:07:27 -0300

    feat: Add Chat page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../pages/Home'
+
+import { useAppSelector } from '../shared/hooks'
+import { Home, Chat } from '../pages'
 
 export const AppRoutes = () => {
+  const { auth } = useAppSelector(state => state.user)
+
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
+      <Route path="/chat" element={auth ? <Chat /> : <Home />} />
 
       <Route path="*" element={<Home />} />
     </Routes>
