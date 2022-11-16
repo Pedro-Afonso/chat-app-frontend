49a1511aa2e8db059d7928b2c7292c62b95a676f
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-16 09:22:58 -0300

    feat: Implement the AppNavBar and AppDrawer

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react'
+import { AppDrawer } from '../../shared/components/AppDrawer'
+import { AppNavBar } from '../../shared/components/AppNavBar'
+
 export const Chat = () => {
-  return <></>
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
+
+  const userList = [
+    { name: 'José', email: 'jose@email.com' },
+    { name: 'Maria', email: 'maria@email.com' },
+    { name: 'Joao', email: 'joao@email.com' }
+  ]
+  return (
+    <>
+      <AppNavBar
+        setAnchorElNav={setAnchorElNav}
+        anchorElUser={anchorElUser}
+        setAnchorElUser={setAnchorElUser}
+      />
+      <AppDrawer
+        setAnchorElNav={setAnchorElNav}
+        anchorElNav={anchorElNav}
+        userList={userList}
+      />
+    </>
+  )
 }
