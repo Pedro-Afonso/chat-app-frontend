817b2eb0107995eb837fcb7a9e81b9d3d07aa3a8
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-16 09:21:32 -0300

    feat: Add AppSearchBar component

---
+++ src/shared/components/AppSearchBar/index.tsx
@@ -0,0 +1,48 @@
+import { styled, alpha } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import SearchIcon from '@mui/icons-material/Search'
+
+const SearchField = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  width: '100%'
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%'
+  }
+}))
+
+export const AppSearchBar = () => {
+  return (
+    <SearchField>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Digite o nome ou email..."
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </SearchField>
+  )
+}
