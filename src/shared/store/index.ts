5b15e91a4bb330602ec3dfd101cf8bcc101655e3
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-16 23:21:33 -0300

    feat: Implement chat reducer to store

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { userReducer } from '../slices/userSlice'
+import { chatReducer } from '../slices/chatSlice'
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    chat: chatReducer
   }
 })
 
