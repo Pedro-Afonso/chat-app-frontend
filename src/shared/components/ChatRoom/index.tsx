a6b7df9fb80a46131939ff384884eef18dee9a02
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-18 11:06:54 -0300

    feat: Improve UI

--- src/shared/components/ChatRoom/index.tsx
+++ src/shared/components/ChatRoom/index.tsx
@@ -1,4 +1,8 @@
+import Details from '@mui/icons-material/RemoveRedEye'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Send from '@mui/icons-material/Send'
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 
@@ -23,9 +27,59 @@ export const ChatRoom: React.FC<IChatRoomProps> = ({ chat }) => {
         justifyContent="center"
         alignItems="center"
       >
-        <Typography fontSize="2rem" textAlign="center">
-          {chat}
-        </Typography>
+        {chat && (
+          <Box display="flex" flexDirection="column" height="100%" width="100%">
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              paddingY={1}
+              paddingX={3}
+            >
+              <Typography fontSize="2rem">{chat}</Typography>
+              <IconButton>
+                <Details />
+              </IconButton>
+            </Box>
+            <Box
+              paddingX={2}
+              paddingBottom={2}
+              flexGrow={1}
+              display="flex"
+              gap={1}
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box component={Paper} elevation={24} flexGrow={1}></Box>
+              <Box component="form" display="flex" alignContent="center">
+                <Box
+                  component={Paper}
+                  flexGrow={1}
+                  variant="elevation"
+                  elevation={24}
+                >
+                  <TextField
+                    autoComplete="off"
+                    multiline={true}
+                    maxRows={4}
+                    fullWidth
+                    placeholder="Envie uma mensagem..."
+                  />
+                </Box>
+                <Box display="flex" alignItems="center">
+                  <IconButton size="large">
+                    <Send />
+                  </IconButton>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        )}
+        {!chat && (
+          <Typography fontSize="2rem" textAlign="center">
+            Clique em um usuário e inicie uma conversa
+          </Typography>
+        )}
       </Box>
     </Box>
   )
