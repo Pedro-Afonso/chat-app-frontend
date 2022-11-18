9f79c8e7ec710fb1629fbc521eb60850b8ffe8d9
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-18 09:19:39 -0300

    feat: Add createGroupChat service

--- src/shared/services/chatService.tsx
+++ src/shared/services/chatService.tsx
@@ -1,7 +1,8 @@
-import { TGetAllChatsByUserRes } from '../interface'
+import { TGetAllChatsByUserRes, TCreateGroupChatRes } from '../interface'
 import { tryCatchService } from '../utils'
 import { Api } from './api'
 
+// Fetch all users chat
 const getAllChatsByUser = (token: string) => {
   return tryCatchService(async () => {
     const config = {
@@ -15,4 +16,28 @@ const getAllChatsByUser = (token: string) => {
   })
 }
 
-export const chatService = { getAllChatsByUser }
+// Create a new group chat
+const createGroupChat = (token: string, name: string, users: string[]) => {
+  return tryCatchService(async () => {
+    const postData = {
+      name,
+      users
+    }
+
+    const config = {
+      headers: {
+        Authorization: `Basic ${token}`
+      }
+    }
+
+    const { data } = await Api.post<TCreateGroupChatRes>(
+      '/api/chats/group/',
+      postData,
+      config
+    )
+
+    return data
+  })
+}
+
+export const chatService = { getAllChatsByUser, createGroupChat }
