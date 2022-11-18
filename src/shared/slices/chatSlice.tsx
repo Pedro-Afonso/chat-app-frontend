d6c35a8e6eaa6c970b3cc12efba956c0fd30fe33
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-18 09:19:48 -0300

    feat: Add createGroupChat slice

--- src/shared/slices/chatSlice.tsx
+++ src/shared/slices/chatSlice.tsx
@@ -11,7 +11,7 @@ const initialState: IChatState = {
   loading: false
 }
 
-// Register a user and sign in
+// Fetch all users chat
 export const getAllChatsByUser = createAsyncThunk<
   TChat[],
   void,
@@ -28,6 +28,31 @@ export const getAllChatsByUser = createAsyncThunk<
 
   return res
 })
+
+// Create a new group chat
+export const createGroupChat = createAsyncThunk<
+  { chat: TChat; message: string },
+  { name: string; users: string[] },
+  { rejectValue: string }
+>(
+  'chat/creategroupchat',
+  async ({ name, users }, { rejectWithValue, getState }) => {
+    const { user } = getState() as { user: IAuthState }
+
+    const res = await chatService.createGroupChat(
+      user.auth?.token || '',
+      name,
+      users
+    )
+
+    // Check for errors
+    if ('errors' in res) {
+      return rejectWithValue(res.errors[0])
+    }
+
+    return res
+  }
+)
 
 export const chatSlice = createSlice({
   name: 'chat',
@@ -49,6 +74,21 @@ export const chatSlice = createSlice({
         state.success = true
       })
       .addCase(getAllChatsByUser.rejected, (state, action) => {
+        state.error = action.payload ? action.payload : null
+        state.loading = false
+      })
+      .addCase(createGroupChat.pending, state => {
+        state.error = null
+        state.success = false
+        state.loading = false
+      })
+      .addCase(createGroupChat.fulfilled, (state, action) => {
+        state.chats.unshift(action.payload.chat)
+        state.chat = action.payload.chat
+        state.loading = false
+        state.success = true
+      })
+      .addCase(createGroupChat.rejected, (state, action) => {
         state.error = action.payload ? action.payload : null
         state.loading = false
       })
