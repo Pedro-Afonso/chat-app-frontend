1e0ed666fe9b0f59d953def688b0e97452c06fcd
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-19 18:26:24 -0300

    feat: Implement send message functionality

--- src/shared/components/SendMessageForm/index.tsx
+++ src/shared/components/SendMessageForm/index.tsx
@@ -5,12 +5,43 @@ import TextField from '@mui/material/TextField'
 import Send from '@mui/icons-material/Send'
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { sendMessage } from '../../slices/messageSlice'
 
 export const SendMessageForm = () => {
+  const dispatch = useAppDispatch()
+  const chat = useAppSelector(state => state.chat.chat)
+
   const [message, setMessage] = useState('')
 
+  const canSendMessage = !!chat && !!message.trim()
+
+  const handleSubmitMessage = () => {
+    if (!canSendMessage) return
+    dispatch(sendMessage({ chatId: chat._id, content: message }))
+    setMessage('')
+  }
+
+  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.code === 'Enter' && e.shiftKey === false) {
+      e.preventDefault()
+      handleSubmitMessage()
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    handleSubmitMessage()
+  }
+
   return (
-    <Box component="form" display="flex" alignContent="center">
+    <Box
+      component="form"
+      display="flex"
+      alignContent="center"
+      onSubmit={handleSubmit}
+      onKeyDown={handleOnKeyDown}
+    >
       <Box component={Paper} flexGrow={1} variant="elevation" elevation={24}>
         <TextField
           autoComplete="off"
@@ -23,7 +54,7 @@ export const SendMessageForm = () => {
         />
       </Box>
       <Box display="flex" alignItems="center">
-        <IconButton size="large">
+        <IconButton type="submit" disabled={!canSendMessage} size="large">
           <Send />
         </IconButton>
       </Box>
