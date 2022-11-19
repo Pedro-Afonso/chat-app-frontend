a726a1e2915bdb9fb7ae1e4a49a99e2fc68b7563
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-19 16:55:28 -0300

    feat: Implement message reducer

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import { userReducer } from '../slices/userSlice'
 import { chatReducer } from '../slices/chatSlice'
+import { messageReducer } from '../slices/messageSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    chat: chatReducer
+    chat: chatReducer,
+    message: messageReducer
   }
 })
 
