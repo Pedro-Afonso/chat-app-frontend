00509cbcdfae2dab78836476e03509097a38a839
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-21 12:08:39 -0300

    feat: Add 'typing' and 'stop typing' socket event

--- src/shared/components/SendMessageForm/index.tsx
+++ src/shared/components/SendMessageForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import IconButton from '@mui/material/IconButton'
 import TextField from '@mui/material/TextField'
@@ -7,6 +7,8 @@ import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import { useAppDispatch, useAppSelector } from '../../hooks'
 import { sendMessage } from '../../slices/messageSlice'
+import { useSocket } from '../../hooks/useSocket'
+import { TChat } from '../../interface'
 
 export const SendMessageForm = () => {
   const dispatch = useAppDispatch()
@@ -14,11 +16,47 @@ export const SendMessageForm = () => {
 
   const [message, setMessage] = useState('')
 
+  const socket = useSocket()
   const canSendMessage = !!chat && !!message.trim()
+
+  const lastChat = useRef<TChat | null>(null)
+  const typing = useRef(false)
+
+  useEffect(() => {
+    if (!chat) return
+
+    if (!lastChat.current) return
+
+    if (!typing.current) {
+      typing.current = true
+      socket.emit('typing', chat._id)
+    }
+
+    const typingTimer = setTimeout(() => {
+      typing.current = false
+      socket.emit('stop typing', chat._id)
+    }, 3000)
+
+    return () => {
+      clearTimeout(typingTimer)
+    }
+  }, [message, socket, chat])
+
+  useEffect(() => {
+    lastChat.current = chat
+    return () => {
+      if (!!lastChat.current && typing.current === true) {
+        typing.current = false
+        socket.emit('stop typing', lastChat.current._id)
+      }
+      lastChat.current = null
+    }
+  }, [chat, socket])
 
   const handleSubmitMessage = () => {
     if (!canSendMessage) return
     dispatch(sendMessage({ chatId: chat._id, content: message }))
+
     setMessage('')
   }
 
