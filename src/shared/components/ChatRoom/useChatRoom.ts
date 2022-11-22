86242f45375e97572fbf054ab0ed4d4b1df14242
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-22 20:35:20 -0300

    feat: Remove removeChat functionality

--- src/shared/components/ChatRoom/useChatRoom.ts
+++ src/shared/components/ChatRoom/useChatRoom.ts
@@ -1,24 +1,18 @@
 import { useEffect } from 'react'
 
-import { useAppDispatch, useAppSelector } from '../../hooks'
-import { removeChat } from '../../slices/chatSlice'
+import { useAppSelector } from '../../hooks'
+
 import { useSocket } from '../../hooks/useSocket'
 
 export const useChatRoom = () => {
   const chat = useAppSelector(state => state.chat.chat)
   const authUser = useAppSelector(state => state.user.auth)
 
-  const dispatch = useAppDispatch()
-
   const socket = useSocket()
 
   const contact = chat?.users.filter(
     contact => contact._id !== authUser?._id
   )[0]
-
-  const handleRemoveChat = () => {
-    dispatch(removeChat())
-  }
 
   useEffect(() => {
     if (chat) {
@@ -27,7 +21,6 @@ export const useChatRoom = () => {
   }, [chat, socket])
   return {
     chat,
-    contact,
-    handleRemoveChat
+    contact
   }
 }
