4885b012ec0497a30566de1f0270dcffc2a90aaf
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-23 19:25:34 -0300

    feat: Add VITE_APP_API_CHAT_URL

--- src/shared/Contexts/SocketContext.tsx
+++ src/shared/Contexts/SocketContext.tsx
@@ -2,7 +2,9 @@
 import { createContext } from 'react'
 import io, { Socket } from 'socket.io-client'
 
-export const socket = io('http://localhost:5000')
+export const socket = io(
+  import.meta.env.VITE_APP_API_CHAT_URL || 'http://localhost:5000'
+)
 
 export const SocketContext = createContext(socket as Socket)
 interface IAppSocketProviderProps {
