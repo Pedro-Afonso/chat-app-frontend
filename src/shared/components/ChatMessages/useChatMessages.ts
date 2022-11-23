3b37e56b3381083788ebcb61789c7bd5ab5bc1f2
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-23 12:20:57 -0300

    refactor: Move logic from useChatMessages to useChatRoom

--- src/shared/components/ChatMessages/useChatMessages.ts
+++ src/shared/components/ChatMessages/useChatMessages.ts
@@ -1,44 +1,9 @@
-import { useEffect, useCallback } from 'react'
-
-import { getAllMessages, receivedMessage } from '../../slices/messageSlice'
-import { useAppSelector, useAppDispatch } from '../../hooks'
-import { useSocket } from '../../hooks/useSocket'
-import { TMessage } from '../../interface'
-import { addNotification } from '../../slices/notificationSlice'
+import { useAppSelector } from '../../hooks'
 
 export const useChatMessages = () => {
-  const dispatch = useAppDispatch()
   const userAuth = useAppSelector(state => state.user.auth)
-  const chat = useAppSelector(state => state.chat.chat)
+
   const messages = useAppSelector(state => state.message.chatMessages)
-
-  const socket = useSocket()
-
-  const handleMessageReceived = useCallback(
-    (newMessage: TMessage) => {
-      if (!newMessage) return
-      if (chat?._id === newMessage.chat._id) {
-        dispatch(receivedMessage(newMessage))
-      } else {
-        dispatch(addNotification(newMessage))
-      }
-    },
-    [dispatch, chat]
-  )
-
-  useEffect(() => {
-    if (chat) {
-      dispatch(getAllMessages(chat._id))
-    }
-  }, [dispatch, chat])
-
-  useEffect(() => {
-    socket.on('message received', handleMessageReceived)
-
-    return () => {
-      socket.off('message received', handleMessageReceived)
-    }
-  }, [socket, handleMessageReceived])
 
   return {
     userAuth,
--- src/shared/components/ChatRoom/useChatRoom.ts
+++ src/shared/components/ChatRoom/useChatRoom.ts
@@ -1,15 +1,49 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useCallback, useRef } from 'react'
 
-import { useAppSelector } from '../../hooks'
+import { useAppDispatch, useAppSelector } from '../../hooks'
 
 import { useSocket } from '../../hooks/useSocket'
+import { TMessage } from '../../interface'
+import { getAllMessages, receivedMessage } from '../../slices/messageSlice'
+import {
+  addNotification,
+  removeNotification
+} from '../../slices/notificationSlice'
 
 export const useChatRoom = () => {
   const chat = useAppSelector(state => state.chat.chat)
   const authUser = useAppSelector(state => state.user.auth)
   const prevChat = useRef('')
 
+  const dispatch = useAppDispatch()
+
   const socket = useSocket()
+
+  const handleMessageReceived = useCallback(
+    (newMessage: TMessage) => {
+      if (!newMessage) return
+      if (chat?._id === newMessage.chat._id) {
+        dispatch(receivedMessage(newMessage))
+      } else {
+        dispatch(addNotification(newMessage))
+      }
+    },
+    [dispatch, chat]
+  )
+
+  useEffect(() => {
+    if (chat) {
+      dispatch(getAllMessages(chat._id))
+    }
+  }, [dispatch, chat])
+
+  useEffect(() => {
+    socket.on('message received', handleMessageReceived)
+
+    return () => {
+      socket.off('message received', handleMessageReceived)
+    }
+  }, [socket, handleMessageReceived])
 
   const contact = chat?.users.filter(
     contact => contact._id !== authUser?._id
@@ -20,8 +54,11 @@ export const useChatRoom = () => {
     if (prevChat.current === chat._id) return
 
     socket.emit('join chat', { chatId: chat._id, leave: prevChat.current })
+
+    dispatch(removeNotification({ chatId: chat._id }))
+
     prevChat.current = chat._id
-  }, [chat, socket])
+  }, [chat, dispatch, socket])
 
   return {
     chat,
