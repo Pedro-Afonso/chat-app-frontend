ea1e99653e37399ab82096aaf9fac70be5b85677
Pedro-Afonso <pedro.766@hotmail.com>
2022-12-14 22:13:10 -0300

    feat: Implement the notification menu component

--- src/shared/components/AppNavBar/NotificationButton/index.tsx
+++ src/shared/components/AppNavBar/NotificationButton/index.tsx
@@ -3,31 +3,58 @@ import MailIcon from '@mui/icons-material/Mail'
 import Tooltip from '@mui/material/Tooltip'
 import Badge from '@mui/material/Badge'
 
-import { INotification } from '../../../slices/notificationSlice'
+import { Notifications } from '../Menu'
+import { useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../hooks'
+import { selectChat } from '../../../slices/chatSlice'
 
-interface INotificationButtonProps {
-  notifications: INotification[]
-  handleOpenNotMenu: () => void
-}
+export const NotificationButton = () => {
+  const [anchorElNotification, setAnchorElNotification] =
+    useState<null | HTMLElement>(null)
 
-export const NotificationButton: React.FC<INotificationButtonProps> = ({
-  notifications,
-  handleOpenNotMenu
-}) => {
+  const dispatch = useAppDispatch()
+
+  const notifications = useAppSelector(
+    state => state.notification.notifications
+  )
+
+  const handleCloseNotificationMenu = () => {
+    setAnchorElNotification(null)
+  }
+
+  const handleOpenNotificationMenu = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNotification(e.currentTarget)
+  }
+
+  const handleClickNotification = (chatId: string) => {
+    return () => {
+      dispatch(selectChat(chatId))
+      handleCloseNotificationMenu()
+    }
+  }
+
   return (
-    <Tooltip title="Notificações">
-      <span>
-        <IconButton
-          size="large"
-          color="inherit"
-          disabled={notifications.length === 0}
-          onClick={handleOpenNotMenu}
-        >
-          <Badge badgeContent={notifications.length} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-      </span>
-    </Tooltip>
+    <>
+      <Tooltip title="Notificações">
+        <span>
+          <IconButton
+            size="large"
+            color="inherit"
+            disabled={notifications.length === 0}
+            onClick={handleOpenNotificationMenu}
+          >
+            <Badge badgeContent={notifications.length} color="error">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+        </span>
+      </Tooltip>
+      <Notifications
+        notifications={notifications}
+        anchorElNotification={anchorElNotification}
+        handleClickNotification={handleClickNotification}
+        handleCloseNotificationMenu={handleCloseNotificationMenu}
+      />
+    </>
   )
 }
